1494e5a7ceec5cc90bef5c7feef7c2fec74dae8a
satoruk <koyanagi3106@gmail.com>
2021-08-18 12:52:22 +0900

    Add ApplicationService
    - Issue #5

---
+++ spec/services/application_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ::ApplicationService, type: :service do
+  let(:test_class) do
+    Class.new(::ApplicationService) do
+      def self.name
+        'SampleService'
+      end
+
+      def call
+        'Sample'
+      end
+    end
+  end
+
+  it 'ApplicationService.call' do
+    expect(test_class.call).to eql("Sample")
+  end
+end
