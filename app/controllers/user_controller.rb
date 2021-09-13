fd1d474c1bf83f08757f2ba4a9277d126cc6fa5b
satoruk <koyanagi3106@gmail.com>
2021-09-13 13:37:37 +0900

    WIP

--- app/controllers/user_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-  def create()
-    render json: {status: 200}
-  end
-end
---
+++ app/domains/users/users_domain.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Users
+  class UsersDomain < Domain
+    def call
+      # TODO
+    end
+  end
+end
