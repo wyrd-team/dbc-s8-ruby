a6f4b5a4bad5dc447a1d05289c0d234f7a600a79
satoru koyanagi <koyanagi3106@gmail.com>
2021-10-13 04:45:38 +0000

    WIP

---
+++ spec/domains/users/user_acl_domain_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::Users::UserAclDomain, type: :domain do
+  context '.can_create_user?' do
+    if '' do
+      described_class.can_create_user?()
+    end
+  end
+  context '.can_update_user?' do
+    if '' do
+    end
+  end
+  context '.can_delete_user?' do
+    if '' do
+    end
+  end
+end
