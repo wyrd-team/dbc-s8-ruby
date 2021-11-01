bf1160725012d4c8f6a1ba73c40c3b40551cc77b
satoru koyanagi <koyanagi3106@gmail.com>
2021-11-01 04:11:29 +0000

    Add scaffold tasks

---
+++ app/domains/tasks/task_acl_domain.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Tasks
+  class TaskAclDomain < ::Domain
+  end
+end
---
+++ app/repositories/tasks/task_repository.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Tasks
+  class TaskRepository < ::ApplicationRepository
+  end
+end
